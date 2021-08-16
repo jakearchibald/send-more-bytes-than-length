d6c2a9724771aa9906810d91fa0e162a43e873b8
Jake Archibald <jaffathecake@gmail.com>
2021-08-16 10:30:30 +0100

    demo

---
+++ server.js
@@ -0,0 +1,36 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
+
+const path = require("path");
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+
+// Setup our static files
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+fastify.get("/bigger-than-content-length", async (req, reply) => {
+  reply.headers({
+    "Content-Type": "text/plain",
+    "Content-Length": "1000",
+  });
+  reply.send("0".repeat(1500));
+});
+
+// Run the server and report out to the logs
+fastify.listen(process.env.PORT, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastify.log.info(`server listening on ${address}`);
+});
